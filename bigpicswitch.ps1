c95b18575661aa5ba9a9c5fa96406337c11d31f2
ken <ken@kenherbin.com>
2023-06-28 00:32:54 -0400

    bugfixes

--- bigpicswitch.ps1
+++ bigpicswitch.ps1
@@ -10,7 +10,6 @@ $multimonitortoolPath = $config.multimonitortoolPath
 $tvMonitorConfig = $config.tvMonitorConfig
 $desktopMonitorConfig = $config.desktopMonitorConfig
 $PCsource = $config.PCsource
-$TVsource = $config.TVsource
 
 $global:retries = $config.retries
 $global:entityID = $config.entityID
@@ -29,10 +28,10 @@ function SendHomeAssistantCommand{
         [Parameter(Mandatory=$true)][string]$method, 
         [Parameter(Mandatory=$true)][hashtable]$header, 
         [hashtable]$body)
-    $bodyJSON = $body | ConvertTo-Json
     $fullURI = $homeAssistantURI + $uri
     try {
         if($body){
+            $bodyJSON = $body | ConvertTo-Json
             $response = Invoke-RestMethod -Uri $fullURI -Method $method -Headers $header -body $bodyJSON
         }else{
             $response = Invoke-RestMethod -Uri $fullURI -Method $method -Headers $header 
@@ -41,7 +40,7 @@ function SendHomeAssistantCommand{
     catch {
         Write-Output "Error: $response"
     }
-    return $reponse
+    return $response
 }
 function checkState{
     $method = "GET"
@@ -63,20 +62,33 @@ function turnOn{
             "entity_id" = $entityID
         }
     $counter = 0
+    $state = checkState
     if($counter -lt $retries){
-        while($checkState -eq "off"){
+        while($state -eq "off"){
             SendHomeAssistantCommand -uri $turnOnURI -method $method -header $header -body $body
             Start-Sleep -Seconds 5
             $counter++
+            $state = checkState
         }
+    }
 }
 function turnOff{
     $method = "POST"
     $body = @{
             "entity_id" = $entityID
         }
-    SendHomeAssistantCommand -uri $turnOffURI -method $method -header $header -body $body
+    $counter = 0
+    $state = checkState
+    if($counter -lt $retries){
+        while($state -eq "on"){
+            SendHomeAssistantCommand -uri $turnOffURI -method $method -header $header -body $body
+            Start-Sleep -Seconds 5
+            $counter++
+            $state = checkState
+        }
+    }
 }
+
 function bigpicture{
     # Change Windows 11 monitor configuration
     Start-Process -FilePath $multimonitortoolPath -ArgumentList "/loadconfig", $tvMonitorConfig
@@ -88,19 +100,14 @@ function bigpicture{
     # Launch Steam Big Picture Mode
     Start-Process -FilePath "steam://open/bigpicture" -Wait
 }
-
-    
-    
-}
 function desktop{
     # Exit Steam Big Picture Mode
     Start-Process -FilePath "steam://close/bigpicture" -Wait
     # Change Windows 11 monitor configuration
     Start-Process -FilePath $multimonitortoolPath -ArgumentList "/loadconfig", $desktopMonitorConfig
-    # Switch Samsung Q90 TV input to HDMI
-    switchInput -source $TVsource
     # Turn off Samsung Q90 TV
     turnOff
+
 }
 if($action -eq "bigpicture"){
     bigpicture
